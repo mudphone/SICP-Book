6d9add5605560b44d4f6baad12a14ce6cda2bea8
Kyle Oba <koba@mudphone.com>
2017-06-13 00:34:03 -1000

    Add allowance for different coin denominations

--- src/app/chapter2_2_1.cljs
+++ src/app/chapter2_2_1.cljs
@@ -20,3 +20,25 @@
     (go nil xs)))
 
 (defc reverse-list-123 (list-str (reverse' (list' 1 2 3))))
+
+;; Exercise 2.19
+(def us-coins (list' 50 25 10 5 1))
+(def uk-coins (list' 100 50 20 10 5 2 1 0.5))
+
+(def first-denomination car')
+
+(def except-first-denomination cdr')
+
+(def no-more? nil?)
+
+(defn cc [amount coin-values]
+  (cond
+    (= amount 0) 1
+    (or (< amount 0) (no-more? coin-values)) 0
+    :else (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomination coin-values))
+                 coin-values))))
+
+(defc cc-100-us-coins (cc 100 us-coins))
