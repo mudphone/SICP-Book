816fbe503a478c620320533a7c6ce3c8652b24a2
Kyle Oba <koba@mudphone.com>
2017-06-16 20:48:03 -1000

    Ex2.29: two implementations of binary mobiles

--- src/app/chapter2_2_1.cljs
+++ src/app/chapter2_2_1.cljs
@@ -114,3 +114,106 @@
 
 (defc fringe-1-6 (list-str (fringe (list' (list' 1 (list' 2 3) 4) (list' 5 6)))))
 
+;; Exercise 2.29
+;; (defn make-mobile [left right]
+;;   (list' left right))
+
+;; (defn make-branch [length structure]
+;;   (list' length structure))
+
+;; (defn is-mobile? [m]
+;;   (pair? m))
+
+(defn make-mobile [left right]
+  [left right])
+
+(defn make-branch [length structure]
+  [length structure])
+
+(defn is-mobile? [m]
+  (and (not (int? m))
+       (seq m)))
+
+;; a)
+(defmulti left-branch (fn [mobile] (type mobile)))
+(defmethod left-branch PersistentVector
+  [mobile]
+  (first mobile))
+(defmethod left-branch app.pair/Pair
+  [mobile]
+  (car' mobile))
+
+(defmulti right-branch (fn [mobile] (type mobile)))
+(defmethod right-branch PersistentVector
+  [mobile]
+  (last mobile))
+(defmethod right-branch app.pair/Pair
+  [mobile]
+  (car' (cdr' mobile)))
+
+(defmulti branch-length (fn [branch] (type branch)))
+(defmethod branch-length PersistentVector
+  [branch]
+  (first branch))
+(defmethod branch-length app.pair/Pair
+  [branch]
+  (car' branch))
+
+(defmulti branch-structure (fn [branch] (type branch)))
+(defmethod branch-structure PersistentVector
+  [branch]
+  (last branch))
+(defmethod branch-structure app.pair/Pair
+  [branch]
+  (car' (cdr' branch)))
+
+;; b)
+(declare total-weight)
+(defn branch-weight [branch]
+  (let [s (branch-structure branch)]
+    (if (not (is-mobile? s))
+      s
+      (total-weight s) )))
+
+(defn total-weight [mobile]
+  (+ (branch-weight (left-branch mobile))
+     (branch-weight (right-branch mobile))))
+
+(def mobile (make-mobile
+             (make-branch 1 (make-mobile
+                             (make-branch 2 20)
+                             (make-branch 3 30)))
+             (make-branch 4 (make-mobile
+                             (make-branch 5 50)
+                             (make-branch 6 (make-mobile
+                                             (make-branch 7 70)
+                                             (make-branch 8 80)))))))
+
+(defc total-weight-mobile (total-weight mobile))
+
+;; c)
+(defn torque [branch]
+  (* (branch-length branch) (branch-weight branch)))
+
+(defn balanced [mobile]
+  (let [go (fn [m acc]
+             (cond
+               (false? acc) false
+               :else
+               (let [left-br (left-branch m)
+                     right-br (right-branch m)
+                     left-br-has-mobile? (is-mobile? (branch-structure left-br))
+                     right-br-has-mobile? (is-mobile? (branch-structure right-br))]
+                 (and
+                  (= (torque left-br) (torque right-br))
+                  (if left-br-has-mobile?
+                    (balanced (branch-structure left-br))
+                    true)
+                  (if right-br-has-mobile?
+                    (balanced (branch-structure right-br))
+                    true)))))]
+    (go mobile true)))
+
+(def balanced-mobile (make-mobile (make-branch 1 4) (make-branch 1 (make-mobile (make-branch 2 2) (make-branch 2 2)))))
+
+(defc balanced-mobile-14-22-22 (str (balanced balanced-mobile)))
